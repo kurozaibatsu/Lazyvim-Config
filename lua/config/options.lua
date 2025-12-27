b3f37860d2b37ce892b6eeec1c77e9a93a307f82
Raphael Lamp <raphael.lamp@proton.me>
2025-12-28 00:16:32 +0100

    use login shell

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- use absolute line numbers
 vim.opt.relativenumber = false
+
+-- use login shell
+LazyVim.terminal.setup("bash --login")
