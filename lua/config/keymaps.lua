4bf4b16c94d8ddd8454f5aedd4ae635647d4892e
Raphael Lamp <raphael.lamp@proton.me>
2025-12-29 14:38:20 +0100

    keymaps: add fast scroll

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,13 +5,17 @@
 -- q Save file
 vim.keymap.set({"n", "x"}, "q", ":write<CR>", { noremap = true, silent = true })
 
--- z Redo
-vim.keymap.set("n", "z", ":redo<CR>", { noremap = true, silent = true })
-vim.keymap.set("x", "z", "<Nop>", { noremap = true, silent = true })
+-- fast scroll
+vim.keymap.set({"n", "x"}, "J", "<C-d>", { noremap = true, silent = true })
+vim.keymap.set({"n", "x"}, "K", "<C-u>", { noremap = true, silent = true })
+vim.keymap.set({"n", "x"}, "<C-d>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set({"n", "x"}, "<C-u>", "<Nop>", { noremap = true, silent = true })
+
+-- CTRL+U Redo
+vim.keymap.set("n", "<C-u>", ":redo<CR>", { noremap = true, silent = true })
 
 -- ;; Enter Normal mode
 vim.keymap.set({"x", "i"}, ";;", "<esc>", { noremap = true, silent = true })
 
 -- ;r Reset buffer
 vim.keymap.set("n", ";r", ":edit!<CR>", { noremap = true, silent = true })
-
