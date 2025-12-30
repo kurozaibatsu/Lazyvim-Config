82590388fa6c5cf8e6fa9a009417bf6f04a47aa4
Raphael Lamp <raphael.lamp@proton.me>
2025-12-30 11:44:46 +0100

    keymaps: reset buffer using Q

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- q Save file
-vim.keymap.set({"n", "x"}, "q", ":write<CR>", { noremap = true, silent = true })
+-- q Write buffer to file
+vim.keymap.set({"n", "x"}, "q", "<CMD>write<CR>", { noremap = true, silent = true })
+-- Q Read buffer from file
+vim.keymap.set("n", "Q", "<CMD>edit!<CR>", { noremap = true, silent = true })
 
 -- Scrolling
 vim.keymap.set({"n", "x"}, "<A-j>", "<C-d>", { noremap = true, silent = true })
@@ -11,18 +13,14 @@ vim.keymap.set({"n", "x"}, "<A-k>", "<C-u>", { noremap = true, silent = true })
 vim.keymap.set({"n", "x"}, "<C-d>", "<Nop>", { noremap = true, silent = true })
 vim.keymap.set({"n", "x"}, "<C-u>", "<Nop>", { noremap = true, silent = true })
 
--- CTRL+U Redo
-vim.keymap.set("n", "<C-u>", ":redo<CR>", { noremap = true, silent = true })
+-- CTRL+u Redo
+vim.keymap.set("n", "<C-u>", "<CMD>redo<CR>", { noremap = true, silent = true })
 
 -- ;; Enter Normal mode
-vim.keymap.set({"x", "i"}, ";;", "<esc>", { noremap = true, silent = true })
+vim.keymap.set({"x", "i"}, ";;", "<ESC>", { noremap = true, silent = true })
 
--- CTRL+C Close
-vim.keymap.set("n", "<C-c>", ":qa<CR>", { noremap = true, silent = true })
+-- CTRL+c Close
+vim.keymap.set("n", "<C-c>", "<CMD>quitall<CR>", { noremap = true, silent = true })
 
--- CTRL+C Copy
+-- CTRL+c Copy
 vim.keymap.set("x", "<C-c>", "y", { noremap = true, silent = true })
-
-
--- <leader>r Reset buffer
-vim.keymap.set("n", "<leader>r", ":edit!<CR>", { noremap = true, silent = true, desc = "Reset buffer" })
