cd36bacd019eb91d30647207b177bedbc8b3f058
Raphael Lamp <raphael.lamp@proton.me>
2026-01-03 14:13:34 +0100

    change keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,10 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- q Write buffer to file
-vim.keymap.set({"n", "x"}, "q", "<CMD>write<CR>", { noremap = true, silent = true })
--- Q Read buffer from file
-vim.keymap.set("n", "Q", "<CMD>edit!<CR>", { noremap = true, silent = true })
+-- m Write buffer to file (overrides marks)
+vim.keymap.set({"n", "x"}, "m", "<CMD>write<CR>", { noremap = true, silent = true })
+-- M Read buffer from file (overrides jump to the middle of the screen)
+vim.keymap.set({"n", "x"}, "M", "<CMD>edit!<CR>", { noremap = true, silent = true })
 
 -- Scrolling
 vim.keymap.set({"n", "x"}, "<A-j>", "<C-d>", { noremap = true, silent = true })
@@ -28,9 +28,18 @@ vim.keymap.set("n", "<C-u>", "<CMD>redo<CR>", { noremap = true, silent = true })
 -- ;; Enter Normal mode
 vim.keymap.set({"x", "i"}, ";;", "<ESC>", { noremap = true, silent = true })
 
+-- Cursor movement in insert mode
+vim.keymap.set("i", "<A-h>", "<Left>", { noremap = true, silent = true })
+vim.keymap.set("i", "<A-l>", "<Right>", { noremap = true, silent = true })
+vim.keymap.set("i", "<A-j>", "<Down>", { noremap = true, silent = true })
+vim.keymap.set("i", "<A-k>", "<UP>", { noremap = true, silent = true })
+
+-- CTRL+l delete character to the right to match with CTRL+h
+vim.keymap.set("i", "<C-l>", "<DEL>", { noremap = true, silent = true })
+
+-------------------------------------- Common editor behavior --------------------------------------
 -- CTRL+q Close
 vim.keymap.set({"n", "x", "i"}, "<C-q>", "<CMD>quitall<CR>", { noremap = true, silent = true })
-
 -- CTRL+c Copy
 vim.keymap.set("x", "<C-c>", "y", { noremap = true, silent = true })
 vim.keymap.set("s", "<C-c>", "<C-g>y", { noremap = true, silent = true })
@@ -43,16 +52,16 @@ vim.keymap.set("n", "<C-x>", "dd", { noremap = true, silent = true })
 vim.keymap.set({"n", "x"}, "<C-v>", "p", { noremap = true, silent = true })
 vim.keymap.set("s", "<C-v>", "<C-g>p", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-v>", "<C-r>+", { noremap = true, silent = true })
--- CTRL+A Select all
+-- CTRL+a Select all
 vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true, silent = true })
 vim.keymap.set({"x", "i"}, "<C-a>", "<ESC>ggVG", { noremap = true, silent = true })
 vim.keymap.set("s", "<C-a>", "<C-g><ESC>ggVG", { noremap = true, silent = true })
+-- Shift + arrow-keys select text
+vim.keymap.set("i", "<S-Left>", "<ESC>v<C-g><Left>", { noremap = true, silent = true })
+vim.keymap.set("i", "<S-Right>", "<ESC>v<C-g><Right>", { noremap = true, silent = true })
 
--- Cursor movement in insert mode
-vim.keymap.set("i", "<A-h>", "<Left>", { noremap = true, silent = true })
-vim.keymap.set("i", "<A-l>", "<Right>", { noremap = true, silent = true })
-vim.keymap.set("i", "<A-j>", "<Down>", { noremap = true, silent = true })
-vim.keymap.set("i", "<A-k>", "<UP>", { noremap = true, silent = true })
-
--- CTRL+l delete character to the right to match with CTRL+h
-vim.keymap.set("i", "<C-l>", "<DEL>", { noremap = true, silent = true })
+-------------------------------------- Remove unused keymaps --------------------------------------
+vim.keymap.set({"n", "x"}, "<C-f>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set({"n", "x"}, "<C-b>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set({"n", "x"}, "'", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set({"n", "x"}, "`", "<Nop>", { noremap = true, silent = true })
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    spec = {
+        { "'", hidden = true },
+        { "`", hidden = true },
+    }
+  },
+}
